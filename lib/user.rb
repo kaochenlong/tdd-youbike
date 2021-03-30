f6f4501db9456ef1297ad2574812f3cdefe0ca33
Eddie Kao <eddie@5xruby.tw>
2021-03-30 16:55:32 +0800

    add tests for user

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,10 @@
 class User
+  attr_reader :member
+  def initialize(member: false)
+    @member = member
+  end
+
+  def member?
+    member == true
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,17 @@
 require 'user'
 
 RSpec.describe User do
-  pending '試著動手寫一些測試吧！'
+  context '會員資格' do
+    it '是會員' do
+      user = User.new(member: true)
+
+      expect(user).to be_member
+    end
+
+    it '不是會員（預設值）' do
+      user = User.new
+
+      expect(user).not_to be_member
+    end
+  end
 end
